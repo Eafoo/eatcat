6d756376b0e726c620e042dd9c92ff0e6cd58208
Eafoo <59077775+Eafoo@users.noreply.github.com>
2022-01-26 19:09:46 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -566,9 +566,40 @@ function click(index) {
     gameTapEvent(fakeEvent)
 }
 
-function autoset(asss) {
-    key = asss.split('');
-    len = key.length;
-    cookie('note', note, 100);
-    gameRestart();
+function foreach() {
+    var strCookie = document.cookie;
+    var arrCookie = strCookie.split("; "); // 将多cookie切割为多个名/值对
+    for (var i = 0; i < arrCookie.length; i++) { // 遍历cookie数组，处理每个cookie对
+        var arr = arrCookie[i].split("=");
+        if (arr.length > 0)
+            DelCookie(arr[0]);
+    }
+}
+
+function GetCookieVal(offset) {
+    var endstr = document.cookie.indexOf(";", offset);
+    if (endstr == -1)
+        endstr = document.cookie.length;
+    return decodeURIComponent(document.cookie.substring(offset, endstr));
+}
+function DelCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = GetCookie(name);
+    document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
+}
+
+function GetCookie(name) {
+    var arg = name + "=";
+    var alen = arg.length;
+    var clen = document.cookie.length;
+    var i = 0;
+    while (i < clen) {
+        var j = i + alen;
+        if (document.cookie.substring(i, j) == arg)
+            return GetCookieVal(j);
+        i = document.cookie.indexOf(" ", i) + 1;
+        if (i == 0) break;
+    }
+    return null;
 }
